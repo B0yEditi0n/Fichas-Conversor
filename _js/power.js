100369e1a1bbf7b6c4f2de5a819a3e544fcf4809
caio <ccaioabreudesouza9874@gmail.com>
2022-12-16 11:00:54 -0300

    Adicionado construção de Aflição

--- _js/power.js
+++ _js/power.js
@@ -1,14 +1,21 @@
 //src/Database/Effects.ts
 function callPower(power){    
     buildPower = new chosePower();
-    console.log(power)
-    document.getElementById('poderes').innerHTML += buildPower.startSelect(power)
+    buildPower.loopPower(power)
 }
 
 // função utilizada para construir itens dos arranjos
 function tablePowerItem(powerItens){
     reBuild = new chosePower()
-    return(reBuild.startSelect(powerItens))
+    var html = ''
+    
+    for(i = 0; i <= powerItens.length - 1; i++){  
+        html += (reBuild.startSelect(powerItens[i]))
+        
+    }
+    return(html)
+    
+    
 
 }
 
@@ -16,11 +23,101 @@ class powerLayout{
     //Poderes comuns
     outherPower(nowPower, effect){
         var item_html
-        item_html = `<strong>${nowPower['name']}:</strong> `
-        item_html += effect
+        item_html = `<strong>${nowPower['name']}:</strong> `    //Nome
+        item_html += effect + ' '                               //Efeito
+        item_html += nowPower['rank']                           //Graduação
+        //Extras
+
+        //Falhas
         item_html += `</br>`
-        console.log(item_html)
         return (item_html);
+    }
+    //Aflição
+    afflictionPower(nowPower){
+        var i = 0
+
+        var distanciModify = false;
+        var afflict_html = ''
+        var range_html = ''
+        var extra_html = ''
+        var flaws_html = ''
+        var condit_html = ''        
+
+        afflict_html = `<strong>${nowPower['name']}:</strong> `
+        afflict_html += 'Aflição '
+        afflict_html += `${nowPower['rank']} `
+
+        condit_html = '['
+        if(nowPower['conditions']['firstDegree'] != ''){
+            condit_html += `${nowPower['conditions']['firstDegree']}`
+        if(nowPower['conditions']['secondDegree'] != '')
+            condit_html += `, ${nowPower['conditions']['secondDegree']}`
+        if(nowPower['conditions']['thirdDegree']){
+                    condit_html += `, ${nowPower['conditions']['thirdDegree']}`
+                }
+        }else{ // Efeit de apenas 3º grau
+            condit_html += nowPower['conditions']['thirdDegree']
+        }
+        condit_html += '] '
+        
+        
+
+        //Extras fora distância
+        for(i=0; i <= nowPower['extras'].length - 1; i++){
+            
+            console.log(nowPower['extras'][i]['id'])
+            if (!(nowPower['extras'][i]['id'] == 10017)){
+                
+                extra_html += `${nowPower['extras'][i]['name']}, `
+            }
+            else{                
+                distanciModify = true;
+                
+            }
+        }
+        //Falhas 
+        
+        for(i=0; i <= nowPower['flaws'].length - 1; i++){
+            
+            if (!(nowPower['flaws'][i]['id'] == 11001)){
+                
+
+            flaws_html += `${nowPower['flaws'][i]['name']}, `
+            }
+            else{
+                distanciModify = true;
+            }
+            
+        }      
+        
+        if(distanciModify){
+            
+            console.log(distanciModify)
+            switch(nowPower['rangeID']){
+                case 1:
+                    range_html = 'Corpo-a-corpo'
+                    break; 
+                case 2:
+                    range_html = 'a Distância'
+                    break;
+                case 3:
+                    range_html = 'a Percepção'
+                    break;
+            }
+
+        }
+            
+
+        afflict_html += `${range_html} ${condit_html}, ${nowPower['rangeID']} ${extra_html}, ${flaws_html}`
+
+        
+
+        afflict_html += `</br>`
+        
+        
+        return (afflict_html);
+
+        
     }
     //Poderes combinados pergunte pro bernardo
     multiPower(nowPower){
@@ -30,6 +127,7 @@ class powerLayout{
         table_html += '</td></tr></table>'
         
         return (table_html)
+        
     }
     //Aranjo de Poder
     powerArry(nowPower){
@@ -42,29 +140,44 @@ class powerLayout{
     }
 }
 class chosePower extends powerLayout{
+    loopPower(powers){
+        var html = ''
+        var i
+        for(i=0; i <= (powers.length - 1); i++){             
+            
+            html = this.startSelect(powers[i])
+            document.getElementById('poderes').innerHTML += html
+        }
+
+    }
     startSelect(powers){
         var i 
         var html = ''
-        for(i=0; i <= (powers.length - 1); i++){                     
-            console.log(powers[i]['effectID'])
-            switch (powers[i]['effectID']){
-                case 5046: //Múltiplos Efeitos                
-                    html += this.multiPower(powers[i])
-                    break;
-                case 5042: //Arranjo            
-                    html += this.powerArry(powers[i])                                
-                    break;
-                case 5013: //Dano                    
-                    html += this.outherPower(powers[i], 'Dano')
-                    break;
-                case 5001: // Aflição
-                    break;
-                default:
-                    
-                    html += this.outherPower(powers[i], 'Desonhecido')
-    
-            }      
-        }
+        switch (powers['effectID']){
+            case 5046: //Múltiplos Efeitos                
+                html = this.multiPower(powers)
+                break;
+            case 5042: //Arranjo           
+                html = this.powerArry(powers)                                
+                break;
+//              Lista de EFeitos individuais                    
+            case 5013: //Dano                    
+                html = this.outherPower(powers, 'Dano')
+                break;
+            case 5001: // Aflição
+                html = this.afflictionPower(powers)
+                break;
+            case 5002:
+                html = this.outherPower(powers, 'Alongamento')
+                break;
+            case 5003:
+                html = this.outherPower(powers, 'Ambiente')
+                break
+            default:
+                html = this.outherPower(powers, 'Desonhecido')
+
+        }     
+        
         return (html)
     }
 }
